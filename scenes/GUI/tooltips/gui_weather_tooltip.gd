af314fef5e8e3f1b404831a79967ee8c7a40191e
Chris Yang <cyang8954.game@gmail.com>
2025-07-17 14:07:24 +0800

    weather tooltip

--- scenes/GUI/tooltips/gui_weather_tooltip.gd
+++ scenes/GUI/tooltips/gui_weather_tooltip.gd
@@ -2,6 +2,8 @@ class_name GUIWeatherTooltip
 extends GUITooltip
 
 @onready var _gui_generic_description: GUIGenericDescription = %GUIGenericDescription
+@onready var _name_label: Label = %NameLabel
 
 func update_with_weather_data(weather_data:WeatherData) -> void:
+	_name_label.text = weather_data.display_name
 	_gui_generic_description.update(weather_data.display_name, weather_data.actions, weather_data.get_display_description())
