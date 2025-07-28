d56a79fd8d26fd5d28c8281ecda29b912b3a95c0
Chris Yang <cyang8954.game@gmail.com>
2025-07-28 11:29:09 +0800

    hibiscus icon

---
+++ scenes/main_game/plants/plants/plant_hibiscus.gd
@@ -0,0 +1,11 @@
+class_name PlantHibiscus
+extends Plant
+
+func _has_ability(ability_type:AbilityType) -> bool:
+	return ability_type == AbilityType.HARVEST
+
+func _trigger_ability(ability_type:AbilityType, _main_game:MainGame) -> void:
+	assert(ability_type == AbilityType.HARVEST)
+	await Util.await_for_tiny_time()
+	await Singletons.main_game.draw_cards(data.data["card"] as int)
+	ability_triggered.emit(ability_type)
