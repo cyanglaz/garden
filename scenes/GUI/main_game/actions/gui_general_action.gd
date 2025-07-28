dffa48a3fb97b30a2ba166285c8106624dde4bbf
Chris Yang <cyang8954.game@gmail.com>
2025-07-28 10:31:45 +0800

    cards in hand icon

--- scenes/GUI/main_game/actions/gui_general_action.gd
+++ scenes/GUI/main_game/actions/gui_general_action.gd
@@ -16,16 +16,23 @@ func _ready() -> void:
 
 func update_with_action(action_data:ActionData) -> void:
 	_gui_action_type_icon.update_with_action_type(action_data.type)
-	if action_data.value == 0:
-		_value_icon.hide()
-	else:
-		_value_icon.show()
-		var value_id := str(abs(action_data.value))
-		if action_data.value > 10:
-			value_id = "max"
-		else:
-			var icon_path := VALUE_ICON_PATH + value_id + ".png"
-			_value_icon.texture = load(icon_path)
-			if action_data.value < 0:
-				_sign_icon.show()
-				_sign_icon.texture = load(SIGN_ICON_PATH + "minus.png")
+	match action_data.value_type:
+		ActionData.ValueType.NUMBER:
+			if action_data.value == 0:
+				_value_icon.hide()
+			else:
+				_value_icon.show()
+				var value_id := str(abs(action_data.value))
+				if action_data.value > 10:
+					value_id = "max"
+				else:
+					var icon_path := VALUE_ICON_PATH + value_id + ".png"
+					_value_icon.texture = load(icon_path)
+					if action_data.value < 0:
+						_sign_icon.show()
+						_sign_icon.texture = load(SIGN_ICON_PATH + "minus.png")
+		ActionData.ValueType.NUMBER_OF_TOOL_CARDS_IN_HAND:
+			_sign_icon.show()
+			_value_icon.show()
+			_sign_icon.texture = load(SIGN_ICON_PATH + "equals.png")
+			_value_icon.texture = load(VALUE_ICON_PATH + "cards_in_hand.png")
