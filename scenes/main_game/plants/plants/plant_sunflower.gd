ce59f4f37b31406b0995411191ba118c26030095
Chris Yang <cyang8954.game@gmail.com>
2025-08-22 14:07:50 -0700

    sunflower update

--- scenes/main_game/plants/plants/plant_sunflower.gd
+++ scenes/main_game/plants/plants/plant_sunflower.gd
@@ -7,7 +7,7 @@ func _has_ability(ability_type:AbilityType) -> bool:
 func _trigger_ability(ability_type:AbilityType, main_game:MainGame) -> void:
 	assert(ability_type == AbilityType.END_DAY)
 	var weather_data:WeatherData = main_game.weather_manager.get_current_weather()
-	if weather_data.id == "sunny" && stage == 2:
+	if weather_data.id == "sunny":
 		var action_data:ActionData = ActionData.new()
 		action_data.type = ActionData.ActionType.LIGHT
 		action_data.value = data.data["light"] as int
