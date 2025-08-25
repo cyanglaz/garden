121d45f6d9718babc4d4bfc9bf43c12eae148369
Chris Yang <cyang8954.game@gmail.com>
2025-08-25 16:21:53 -0700

    draft

--- scenes/GUI/tooltips/gui_weather_tooltip.gd
+++ scenes/GUI/tooltips/gui_weather_tooltip.gd
@@ -2,8 +2,12 @@ class_name GUIWeatherTooltip
 extends GUITooltip
 
 @onready var _name_label: Label = %NameLabel
+@onready var _gui_action_list: GUIActionList = %GUIActionList
 @onready var _rich_text_label: RichTextLabel = %RichTextLabel
 
 func update_with_weather_data(weather_data:WeatherData) -> void:
 	_name_label.text = weather_data.display_name
-	_rich_text_label.text = weather_data.get_display_description()
+	if weather_data.actions.is_empty():
+		_rich_text_label.text = weather_data.get_display_description()
+	else:
+		_gui_action_list.update(weather_data.actions)
