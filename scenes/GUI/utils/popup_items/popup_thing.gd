d7824b25c74cce143fd07e769294bad634ccad30
Chris Yang <cyang8954.game@gmail.com>
2025-08-26 10:53:47 -0700

    popup thing bump up

--- scenes/GUI/utils/popup_items/popup_thing.gd
+++ scenes/GUI/utils/popup_items/popup_thing.gd
@@ -1,7 +1,10 @@
 class_name PopupThing
 extends PanelContainer
 
+const BUMP_UP_TIME := 0.2
+
 var _position_tween:Tween
+var _end_position:Vector2
 
 func _ready() -> void:
 	top_level = true
@@ -12,14 +15,15 @@ func animate_show(height:float, spread:float, time:float):
 	global_position = from_global_position
 	var tween = Util.create_scaled_tween(self)
 	var end_x_position := randf_range(-spread, spread)
-	var end_position = global_position + Vector2(end_x_position, -height)
+	_end_position = global_position + Vector2(end_x_position, -height)
 	modulate.a = 0.0
 	_position_tween = Util.create_scaled_tween(self)
-	_position_tween.parallel().tween_property(self, "global_position", end_position, time).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	_position_tween.parallel().tween_property(self, "global_position", _end_position, time).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	#tween.parallel().tween_property(self, "scale", Vector2(final_scale, final_scale), time).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.parallel().tween_property(self, "modulate:a", 1.0, time/2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween.play()
 	await _position_tween.finished
+	_position_tween = null
 
 func animate_destroy(time:float) -> void:
 	var tween = Util.create_scaled_tween(self)
@@ -27,6 +31,15 @@ func animate_destroy(time:float) -> void:
 	tween.play()
 	await tween.finished
 	queue_free()
+
+func bump_up(height:float) -> void:
+	if _position_tween.is_running():
+		_position_tween.kill()
+	_position_tween = Util.create_scaled_tween(self)
+	_position_tween.parallel().tween_property(self, "global_position", _end_position + Vector2(0, -height), BUMP_UP_TIME).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	_position_tween.play()
+	await _position_tween.finished
+	_position_tween = null
 
 func animate_show_and_destroy(height:float, spread:float, show_time:float, destroy_time:float) -> void:
 	await animate_show(height, spread, show_time)
