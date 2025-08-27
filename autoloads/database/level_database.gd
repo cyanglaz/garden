04c5d203ccd41e6060a20a7fba36318a0fd3e517
Chris Yang <cyang8954.game@gmail.com>
2025-08-27 14:46:03 -0700

    specify level in level objet

--- autoloads/database/level_database.gd
+++ autoloads/database/level_database.gd
@@ -3,12 +3,14 @@ extends Database
 
 const DIR = "res://data/levels"
 
-func roll_levels(number_of_levels:int, chapter:int) -> Array[LevelData]:
+func roll_levels(number_of_levels:int, chapter:int, level:int) -> Array[LevelData]:
 	var available_levels:Array = _datas.values().duplicate()
 	var result:Array[LevelData] = []
 	for i in number_of_levels:
 		var level_data:LevelData = available_levels.pick_random()
 		if !level_data.chapters.has(chapter):
+			continue
+		if !level_data.levels.has(level):
 			continue
 		result.append(level_data.get_duplicate())
 		available_levels.erase(level_data)
