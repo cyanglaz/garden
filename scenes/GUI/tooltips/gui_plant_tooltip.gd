5d71cd5c4bec23cdb50d65995e969a9e97df7909
Chris Yang <cyang8954.game@gmail.com>
2025-09-09 20:41:23 -0700

    draft

--- scenes/GUI/tooltips/gui_plant_tooltip.gd
+++ scenes/GUI/tooltips/gui_plant_tooltip.gd
@@ -3,7 +3,7 @@ extends GUITooltip
 
 @onready var _gui_plant_description: GUIPlantDescription = %GUIPlantDescription
 
-var card_tooltips:Array[WeakRef] = []
+var secondary_tooltips:Array[WeakRef] = []
 var _weak_plant_data:WeakRef = weakref(null)
 
 func _ready() -> void:
@@ -19,11 +19,11 @@ func _on_tooltop_shown() -> void:
 	var tool_ids:Array[String] = Util.find_tool_ids_in_data(_weak_plant_data.get_ref().data)
 	for tool_id:String in tool_ids:
 		var tool_data := MainDatabase.tool_database.get_data_by_id(tool_id)
-		card_tooltips.append(weakref(Util.display_card_tooltip(tool_data, self, false, GUITooltip.TooltipPosition.LEFT)))
+		secondary_tooltips.append(weakref(Util.display_card_tooltip(tool_data, self, false, GUITooltip.TooltipPosition.LEFT)))
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE:
-		for weak_card_tooltip in card_tooltips:
-			if weak_card_tooltip.get_ref():
-				weak_card_tooltip.get_ref().queue_free()
-				weak_card_tooltip = weakref(null)
+		for weak_secondary_tooltip in secondary_tooltips:
+			if weak_secondary_tooltip.get_ref():
+				weak_secondary_tooltip.get_ref().queue_free()
+				weak_secondary_tooltip = weakref(null)
