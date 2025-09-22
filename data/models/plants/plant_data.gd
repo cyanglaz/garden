a8bcf1c90a97d070b4b66cd7991c7d4b803acdd6
Chris Yang <cyang8954.game@gmail.com>
2025-09-21 20:36:48 -0700

    roll plants algorithm

--- data/models/plants/plant_data.gd
+++ data/models/plants/plant_data.gd
@@ -7,6 +7,7 @@ const PLANT_SCENE_PATH_PREFIX:String = "res://scenes/main_game/plants/plant_"
 @export var water:int
 @export var immune_to_status:Array[String]
 @export var difficulty:int
+@export var chapters:Array[int]
 
 func copy(other:ThingData) -> void:
 	super.copy(other)
@@ -15,6 +16,7 @@ func copy(other:ThingData) -> void:
 	water = other_plant.water
 	immune_to_status = other_plant.immune_to_status.duplicate()
 	difficulty = other_plant.difficulty
+	chapters = other_plant.chapters.duplicate()
 
 func get_duplicate() -> PlantData:
 	var dup:PlantData = PlantData.new()
