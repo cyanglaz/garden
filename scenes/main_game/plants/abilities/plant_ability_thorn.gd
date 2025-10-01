c3b8ed348ab1704eeb9e741ddb63a48428e32695
Chris Yang <cyang8954.game@gmail.com>
2025-10-01 16:33:34 -0700

    thorn

--- scenes/main_game/plants/abilities/plant_ability_thorn.gd
+++ scenes/main_game/plants/abilities/plant_ability_thorn.gd
@@ -1,11 +1,11 @@
 class_name PlantAbilityThorn
 extends PlantAbility
 
-func _has_ability(ability_type:Plant.AbilityType) -> bool:
+func _has_ability_hook(ability_type:Plant.AbilityType) -> bool:
 	return ability_type == Plant.AbilityType.HARVEST
 
-func _trigger_ability(ability_type:Plant.AbilityType, main_game:MainGame, _plant:Plant) -> void:
+func _trigger_ability_hook(ability_type:Plant.AbilityType, main_game:MainGame, _plant:Plant) -> void:
 	assert(ability_type == Plant.AbilityType.HARVEST)
 	var tool_data:ToolData = MainDatabase.tool_database.get_data_by_id("thorn").get_duplicate()
 	var from_position:Vector2 = Util.get_node_ui_position(main_game.gui_main_game.gui_tool_card_container, self) - GUIToolCardButton.SIZE / 2
-	await main_game.tool_manager.add_temp_tools_to_draw_pile([tool_data], from_position, true, true)
+	await main_game.tool_manager.add_temp_tools_to_discard_pile([tool_data], from_position, true)
--- scenes/main_game/plants/components/plant_ability_container.gd
+++ scenes/main_game/plants/components/plant_ability_container.gd
@@ -1,13 +1,10 @@
 class_name PlantAbilityContainer
 extends Node2D
 
-var abilities:Array[PlantAbilityData]
-
 func setup_with_plant_data(plant_data:PlantData) -> void:
 	for ability_id:String in plant_data.abilities:
 		var ability_data:PlantAbilityData = MainDatabase.plant_ability_database.get_data_by_id(ability_id)
 		var ability_node:PlantAbility = ability_data.get_ability()
-		abilities.append(ability_node)
 		assert(ability_node)
 		add_child(ability_node)
 
@@ -15,7 +12,3 @@ func trigger_ability(ability_type:Plant.AbilityType, main_game:MainGame, plant:P
 	for ability_node:PlantAbility in get_children():
 		if ability_node.has_ability_hook(ability_type):
 			await ability_node.trigger_ability_hook(ability_type, main_game, plant)
-
-func _notification(what:int) -> void:
-	if what == NOTIFICATION_PREDELETE:
-		abilities.clear()
