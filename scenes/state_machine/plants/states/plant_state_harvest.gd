3b9988efa71c3c388d7ecf8cf58cce11dc8e843f
Chris Yang <cyang8954.game@gmail.com>
2025-10-04 11:06:56 -0700

    fixes

--- scenes/state_machine/plants/states/plant_state_harvest.gd
+++ scenes/state_machine/plants/states/plant_state_harvest.gd
@@ -6,7 +6,6 @@ func enter() -> void:
 	plant.harvest_started.emit()
 	await _handle_ability()
 	_play_harvest_animation()
-	await Util.await_for_tiny_time()
 	plant.harvest_completed.emit()
 
 func _play_harvest_animation() -> void:
@@ -14,6 +13,8 @@ func _play_harvest_animation() -> void:
 	exit("")
 
 func _handle_ability() -> void:
+	# Must wait to make the operation async even if the plant does not have any ability hooks
+	await Util.await_for_tiny_time()
 	await plant.trigger_ability(Plant.AbilityType.HARVEST, Singletons.main_game)
 
 func _get_animation_name() -> String:
