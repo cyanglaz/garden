a01e0f277403df7b5ba3736036a28e316a99d23d
Chris Yang <cyang8954.game@gmail.com>
2025-10-25 14:54:05 -0700

    line blink

--- scenes/GUI/map/gui_map_main.gd
+++ scenes/GUI/map/gui_map_main.gd
@@ -1,7 +1,12 @@
 class_name GUIMapMain
 extends CanvasLayer
 
+signal node_button_pressed(node:MapNode)
+
 @onready var gui_map_view: GUIMapView = %GUIMapView
+
+func _ready() -> void:
+	gui_map_view.node_button_pressed.connect(func(node:MapNode) -> void: node_button_pressed.emit(node))
 
 func update_with_map(layers:Array) -> void:
 	gui_map_view.update_with_map.call_deferred(layers)
--- scenes/main_game/map/map_main.gd
+++ scenes/main_game/map/map_main.gd
@@ -5,8 +5,12 @@ signal node_selected(node:MapNode)
 
 @onready var gui: GUIMapMain = %GUIMapMain
 
+#var map_generator:MapGenerator = MapGenerator.new()
+
 func _ready() -> void:
 	gui.node_button_pressed.connect(_on_node_selected)
+	#map_generator.generate(randi())
+	#update_with_map(map_generator.layers)
 
 func update_with_map(layers:Array) -> void:
 	gui.update_with_map(layers)
