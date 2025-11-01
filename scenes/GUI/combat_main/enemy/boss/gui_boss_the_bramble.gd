b90216ca3eba0a47c60e744ca1f0d540b5d2123f
Chris Yang <cyang8954.game@gmail.com>
2025-11-01 09:47:53 -0700

    bramble boss fix

--- scenes/GUI/combat_main/enemy/boss/gui_boss_the_bramble.gd
+++ scenes/GUI/combat_main/enemy/boss/gui_boss_the_bramble.gd
@@ -4,14 +4,14 @@ extends GUIBoss
 func _has_hook(hook_type:HookType) -> bool:
 	return hook_type == HookType.LEVEL_START
 
-func _handle_hook(hook_type:HookType, combat_main:CombatMain) -> void:
+func _handle_hook(hook_type:HookType) -> void:
 	if hook_type != HookType.LEVEL_START:
 		return
 	await Util.await_for_tiny_time()
 	var tool_data:ToolData = MainDatabase.tool_database.get_data_by_id("thorn").get_duplicate()
-	var count:int = boss_data.data["count"] as int
-	var starting_position:Vector2 = combat_main.gui.get_child(0).size/2 - GUIToolCardButton.SIZE/2
+	var count:int = _boss_data.data["count"] as int
+	var starting_position:Vector2 = gui_boss_icon.global_position + Vector2.LEFT * GUIToolCardButton.SIZE.x/2
 	var cards:Array[ToolData] = []
 	for i in count:
 		cards.append(tool_data.get_duplicate())
-	await combat_main.tool_manager.add_temp_tools_to_discard_pile(cards, starting_position, true)
+	await _combat_main.tool_manager.add_temp_tools_to_discard_pile(cards, starting_position, true)
