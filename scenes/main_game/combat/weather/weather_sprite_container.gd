0fc6c46985aebdb8e736efc018a69c04fda6cc59
Chris Yang <cyang8954.game@gmail.com>
2025-11-10 15:15:03 -0800

    cloud

---
+++ scenes/main_game/combat/weather/weather_sprite_container.gd
@@ -0,0 +1,41 @@
+class_name WeatherSpriteContainer
+extends Node2D
+
+signal animated_in_finished()
+signal animated_out_finished()
+
+var _sprites:Array[WeatherSprite]
+var _sprites_animation_count := 0
+
+func _ready() -> void:
+	for child in get_children():
+		assert(child is WeatherSprite, "Child is not a WeatherSprite")
+		child.animated_in_finished.connect(_on_sprite_animated_in_finished)
+		child.animated_out_finished.connect(_on_sprite_animated_out_finished)
+		_sprites.append(child)
+
+func animate_sprites_in() -> void:
+	if _sprites.is_empty():
+		return
+	_sprites_animation_count = _sprites.size()
+	for sprite in _sprites:
+		sprite.animate_in()
+	await animated_in_finished
+
+func animate_sprites_out() -> void:
+	if _sprites.is_empty():
+		return
+	_sprites_animation_count = _sprites.size()
+	for sprite in _sprites:
+		sprite.animate_out()
+	await animated_out_finished
+
+func _on_sprite_animated_in_finished() -> void:
+	_sprites_animation_count -= 1
+	if _sprites_animation_count == 0:
+		animated_in_finished.emit()
+
+func _on_sprite_animated_out_finished() -> void:
+	_sprites_animation_count -= 1
+	if _sprites_animation_count == 0:
+		animated_out_finished.emit()
