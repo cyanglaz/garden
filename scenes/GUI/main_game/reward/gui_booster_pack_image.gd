5e9e193e0ccc5d56d1f7cc39d07ed74ebf0ba656
Chris Yang <cyang8954.game@gmail.com>
2025-11-17 09:11:12 -0800

    draft

--- scenes/GUI/main_game/reward/gui_booster_pack_image.gd
+++ scenes/GUI/main_game/reward/gui_booster_pack_image.gd
@@ -5,7 +5,17 @@ const COMMON_TEXTURE := preload("res://resources/sprites/booster_packs/booster_p
 const RARE_TEXTURE := preload("res://resources/sprites/booster_packs/booster_pack_rare.png")
 const LEGENDARY_TEXTURE := preload("res://resources/sprites/booster_packs/booster_pack_legendary.png")
 
+@onready var shake: Shake = %Shake
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
 var has_outline:bool = true: set = _set_has_outline
+
+func _ready() -> void:
+	play_open_animation()
+
+func play_open_animation() -> void:
+	shake.start(1.0,Vector2(100, 100), 0.01, 0.01, 1)
+	#animation_player.play("dissolve")
 
 func update_with_booster_pack_type(booster_pack_type:ContractData.BoosterPackType) -> void:
 	match booster_pack_type:
