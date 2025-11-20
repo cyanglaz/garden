9f710da52b1c03923a6094658f5aed6e19ece0d8
Chris Yang <cyang8954.game@gmail.com>
2025-11-19 20:38:10 -0800

    chest draft

--- scenes/main_game/chest/chest_main.gd
+++ scenes/main_game/chest/chest_main.gd
@@ -8,12 +8,14 @@ const CHEST_OPEN_DELAY := 0.4
 
 @onready var gui_chest_main: GUIChestMain = %GUIChestMain
 @onready var chest_container: ChestContainer = %ChestContainer
+@onready var weather_main: WeatherMain = %WeatherMain
 
 func _ready() -> void:
 	update_with_number_of_chests(3)
 	chest_container.chest_selected.connect(_on_chest_selected)
 	gui_chest_main.card_reward_selected.connect(_on_card_reward_selected)
 	gui_chest_main.skipped.connect(_on_chest_reward_skipped)
+	weather_main.start(0)
 
 func update_with_number_of_chests(number_of_chests:int) -> void:
 	chest_container.update_with_number_of_chests(number_of_chests)
