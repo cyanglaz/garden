2ba95b5783d3bcf49a7feae33d392aec34dcb24e
Chris Yang <cyang8954.game@gmail.com>
2025-11-28 14:07:32 -0500

    add plants ready initlally

--- scenes/main_game/field/plant_field_container.gd
+++ scenes/main_game/field/plant_field_container.gd
@@ -20,12 +20,11 @@ const PLANT_ICON_OFFSET := Vector2.UP * 4
 var plants:Array[Plant] = []
 var fields:Array[Field] = []
 var mouse_plant:Plant: get = _get_mouse_plant
-var _active_field_index := 0
 var _weak_mouse_plant:WeakRef = weakref(null)
 
-func update_with_number_of_fields(number_of_fields:int) -> void:
+func setup_with_plants(plant_datas:Array) -> void:
 	var current_field:Field = null
-	for i in number_of_fields:
+	for i in plant_datas.size():
 		var field:Field = FIELD_SCENE.instantiate()
 		field.field_hovered.connect(_on_field_hovered.bind(i))
 		field.field_pressed.connect(func(): field_pressed.emit(i))
@@ -34,25 +33,17 @@ func update_with_number_of_fields(number_of_fields:int) -> void:
 		field.action_application_completed.connect(func(): plant_action_application_completed.emit(i))
 		field.index = i
 		_container.add_child(field)
-		field.hide()
 		fields.append(field)
+
+		var plant_data:PlantData = plant_datas[i]
+		field.plant_seed(plant_data)
+		plants.append(field.plant)
+
 		if current_field:
 			field.left_field = current_field
 			current_field.right_field = field
 		current_field = field
 	_layout_fields.call_deferred()
-
-func show_next_fields(number_of_fields:int) -> void:
-	for i in range(_active_field_index, _active_field_index + number_of_fields):
-		fields[i].show()
-		_active_field_index += 1
-
-func plant_seed(plant_data:PlantData) -> void:
-	assert(plants.size() < fields.size(), "Plant index out of bounds")
-	var plant_index := plants.size()
-	var target_field:Field = fields[plant_index]
-	target_field.plant_seed(plant_data)
-	plants.append(target_field.plant)
 
 func trigger_end_turn_hooks(combat_main:CombatMain) -> void:
 	for plant:Plant in plants:
@@ -111,6 +102,12 @@ func get_preview_icon_global_position(preview_icon:Control, index:int) -> Vector
 	var field_position := fields[index].position
 	return Util.get_node_canvas_position(self) + field_position + Vector2.LEFT * preview_icon.size.x/2 + Vector2.UP * preview_icon.size.y + PLANT_ICON_OFFSET
 	
+func are_all_plants_bloom() -> bool:
+	for plant in plants:
+		if !plant.is_bloom():
+			return false
+	return true
+
 func _layout_fields() -> void:
 	if fields.size() == 0:
 		return
