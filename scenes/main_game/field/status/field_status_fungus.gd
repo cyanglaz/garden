649fc14467fce800f7e5a9e5ffef98df52f8f83c
Chris Yang <cyang8954.game@gmail.com>
2025-12-06 06:47:10 -0800

    fungus

--- scenes/main_game/field/status/field_status_fungus.gd
+++ scenes/main_game/field/status/field_status_fungus.gd
@@ -1,7 +1,22 @@
 class_name FieldStatusFungus
 extends FieldStatus
 
-const FUNGUS_SCENE := preload("res://scenes/main_game/field/status/field_status_fungus.tscn")
+const FUNGUS_SCENE := preload("res://scenes/main_game/field/status/status_components/fungus.tscn")
+
+var _number_of_fungi:int = 0
+
+func _update_for_plant(plant:Plant) -> void:
+	var diff_number_of_fungi := stack - _number_of_fungi
+	if diff_number_of_fungi > 0:
+		for i in diff_number_of_fungi:
+			var fungus:Fungus = FUNGUS_SCENE.instantiate()
+			add_child(fungus)
+			fungus.position = _get_random_fungus_position(plant)
+	elif diff_number_of_fungi < 0:
+		for i in range(diff_number_of_fungi):
+			var fungus:Fungus = get_children().back()
+			fungus.queue_free()
+	_number_of_fungi = stack
 
 func _has_end_turn_hook(plant:Plant) -> bool:
 	return plant != null
@@ -12,3 +27,8 @@ func _handle_end_turn_hook(_combat_main:CombatMain, plant:Plant) -> void:
 	reduce_water_action.operator_type = ActionData.OperatorType.DECREASE
 	reduce_water_action.value = (status_data.data["value"] as int) * stack
 	await plant.apply_actions([reduce_water_action])
+
+func _get_random_fungus_position(plant:Plant) -> Vector2:
+	var field_width:float = plant.field.land_width
+	var spawn_range:float = field_width - 4.0
+	return Vector2(randf_range(-spawn_range/2.0, spawn_range/2.0), 0)
