40447236a7132223995416c430f3ba04065e28b4
Chris Yang <cyang8954.game@gmail.com>
2025-12-18 16:46:21 -0800

    dynamic tavern

--- scenes/main_game/town/forge.gd
+++ scenes/main_game/town/forge.gd
@@ -1,2 +1,13 @@
 class_name Forge
 extends Node2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
+
+var highlighted := false: set = _set_highlighted
+
+func _set_highlighted(val:bool) -> void:
+	highlighted = val
+	if highlighted:
+		animated_sprite_2d.material.set_shader_parameter("outline_size", 1)
+	else:
+		animated_sprite_2d.material.set_shader_parameter("outline_size", 0)
--- scenes/main_game/town/forge_field.gd
+++ scenes/main_game/town/forge_field.gd
@@ -3,3 +3,20 @@ extends Field
 
 @onready var forge: Forge = %Forge
 @onready var forge_sign: BuildingSign = %ForgeSign
+
+var _tooltip_id:String = "forge"
+
+func _on_gui_plant_button_mouse_entered() -> void:
+	super._on_gui_plant_button_mouse_entered()
+	forge_sign.highlighted = true
+	forge.highlighted = true
+	var building_name := Util.get_localized_string("FORGE_NAME")
+	var building_description := Util.get_localized_string("FORGE_DESCRIPTION")
+	building_description = DescriptionParser.format_references(building_description, {}, {}, func(_reference_id:String) -> bool: return false)
+	Events.request_display_tooltip.emit(TooltipRequest.new(TooltipRequest.TooltipType.TOWN_BUILDING, null, _tooltip_id, _gui_field_button, GUITooltip.TooltipPosition.BOTTOM, {"name": building_name, "description": building_description}))
+
+func _on_gui_plant_button_mouse_exited() -> void:
+	super._on_gui_plant_button_mouse_exited()
+	forge_sign.highlighted = false
+	forge.highlighted = false
+	Events.request_hide_tooltip.emit(_tooltip_id)
