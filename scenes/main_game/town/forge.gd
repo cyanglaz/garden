b7b5cac1ce3f585835cac7b2eecc60c9f96944ff
Chris Yang <cyang8954.game@gmail.com>
2025-12-18 18:35:27 -0800

    lights

--- scenes/main_game/town/forge.gd
+++ scenes/main_game/town/forge.gd
@@ -2,20 +2,36 @@ class_name Forge
 extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
-@onready var gpu_particles_2d: GPUParticles2D = %GPUParticles2D
+@onready var smoke_particle: GPUParticles2D = %SmokeParticle
+@onready var furnace_spark_particle: GPUParticles2D = %FurnaceSparkParticle
+@onready var fire_particle: GPUParticles2D = %FireParticle
+@onready var point_light_2d: PointLight2D = %PointLight2D
 
 var highlighted := false: set = _set_highlighted
 
 func _ready() -> void:
-	gpu_particles_2d.emitting = false
+	smoke_particle.emitting = false
+	furnace_spark_particle.emitting = false
+	fire_particle.emitting = false
+	point_light_2d.hide()
+
+func _physics_process(_delta: float) -> void:
+	if highlighted:
+		point_light_2d.energy = randf_range(1.3, 1.7)
 
 func _set_highlighted(val:bool) -> void:
 	highlighted = val
 	if highlighted:
 		animated_sprite_2d.material.set_shader_parameter("outline_size", 1)
 		animated_sprite_2d.play("open")
-		gpu_particles_2d.emitting = true
+		smoke_particle.emitting = true
+		furnace_spark_particle.emitting = true
+		fire_particle.emitting = true
+		point_light_2d.show()
 	else:
 		animated_sprite_2d.material.set_shader_parameter("outline_size", 0)
 		animated_sprite_2d.play("closed")
-		gpu_particles_2d.emitting = false
+		smoke_particle.emitting = false
+		furnace_spark_particle.emitting = false
+		fire_particle.emitting = false
+		point_light_2d.hide()
--- scenes/main_game/town/tavern.gd
+++ scenes/main_game/town/tavern.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 @onready var gpu_particles_2d: GPUParticles2D = %GPUParticles2D
+@onready var point_light_2d: PointLight2D = %PointLight2D
 
 var highlighted := false: set = _set_highlighted
 
 func _ready() -> void:
 	gpu_particles_2d.emitting = false
+	point_light_2d.hide()
+
+func _physics_process(_delta: float) -> void:
+	if highlighted:
+		point_light_2d.energy = randf_range(1.3, 1.7)
 
 func _set_highlighted(val:bool) -> void:
 	highlighted = val
@@ -15,7 +21,9 @@ func _set_highlighted(val:bool) -> void:
 		animated_sprite_2d.play("open")
 		animated_sprite_2d.material.set_shader_parameter("outline_size", 1)
 		gpu_particles_2d.emitting = true
+		point_light_2d.show()
 	else:
 		animated_sprite_2d.play("closed")
 		animated_sprite_2d.material.set_shader_parameter("outline_size", 0)
 		gpu_particles_2d.emitting = false
+		point_light_2d.hide()
