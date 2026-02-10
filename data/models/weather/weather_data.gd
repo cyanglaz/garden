bb1111722761cb049b7657f902ca699d78633e99
Chris Yang <cyang8954.game@gmail.com>
2026-02-10 10:54:49 -0800

    weather boss fix

--- data/models/weather/weather_data.gd
+++ data/models/weather/weather_data.gd
@@ -8,6 +8,7 @@ func copy(other:ThingData) -> void:
 	super.copy(other)
 	var other_weather: WeatherData = other as WeatherData
 	sky_color = other_weather.sky_color
+	boss = other_weather.boss
 
 func get_duplicate() -> WeatherData:
 	var dup:WeatherData = WeatherData.new()
