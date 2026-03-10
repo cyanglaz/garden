c24605a352caf6432b44e13cb6b3b1b6c899e2d4
Chris Yang <cyang8954.game@gmail.com>
2026-03-10 10:53:19 -0700

    sun shard

---
+++ scenes/main_game/combat/player/player_upgrades/trinkets/player_trinket_ice_shard.gd
@@ -0,0 +1,13 @@
+class_name PlayerTrinketIceShard
+extends PlayerTrinket
+
+func _has_end_turn_hook(_combat_main:CombatMain) -> bool:
+	return true
+
+func _handle_end_turn_hook(combat_main:CombatMain) -> void:
+	var current_plant:Plant = combat_main.plant_field_container.get_plant(combat_main.player.current_field_index)
+	var action_data:ActionData = ActionData.new()
+	action_data.type = ActionData.ActionType.WATER
+	action_data.operator_type = ActionData.OperatorType.INCREASE
+	action_data.value = data.data["water"] as int
+	await current_plant.apply_actions([action_data])
--- scenes/main_game/combat/player/player_upgrades/trinkets/player_trinket_sun_shard.gd
+++ scenes/main_game/combat/player/player_upgrades/trinkets/player_trinket_sun_shard.gd
@@ -1,11 +1,14 @@
 class_name PlayerTrinketSunShard
 extends PlayerTrinket
 
-func _has_end_turn_hook(_combat_main:CombatMain) -> bool:
-	return true
+func _has_end_turn_hook(combat_main:CombatMain) -> bool:
+	var current_index:int = combat_main.player.current_field_index
+	return current_index == 0 || current_index == combat_main.player.max_plants_index
 
 func _handle_end_turn_hook(combat_main:CombatMain) -> void:
-	var current_plant:Plant = combat_main.plant_field_container.get_plant(combat_main.player.current_field_index)
+	var current_index:int = combat_main.player.current_field_index
+	assert(current_index == 0 || current_index == combat_main.player.max_plants_index, "Current index is not 0 or max plants index")
+	var current_plant:Plant = combat_main.plant_field_container.get_plant(current_index)
 	var action_data:ActionData = ActionData.new()
 	action_data.type = ActionData.ActionType.LIGHT
 	action_data.operator_type = ActionData.OperatorType.INCREASE
