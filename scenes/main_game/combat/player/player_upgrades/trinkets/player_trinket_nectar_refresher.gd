3f9c64f4c5bd83bd11a5c6d974a95849cf37fe36
Chris Yang <cyang8954.game@gmail.com>
2026-03-17 20:30:23 -0700

    nectar refresher update small time

--- scenes/main_game/combat/player/player_upgrades/trinkets/player_trinket_nectar_refresher.gd
+++ scenes/main_game/combat/player/player_upgrades/trinkets/player_trinket_nectar_refresher.gd
@@ -6,5 +6,6 @@ func _has_start_turn_hook(combat_main: CombatMain) -> bool:
 
 func _handle_start_turn_hook(combat_main: CombatMain) -> void:
 	assert(combat_main.day_manager.day == 2, "Nectar Refresher should only trigger on turn 3")
+	await Util.await_for_small_time()
 	combat_main.player.player_status_container.update_player_upgrade(
 		"momentum", int(data.data[&"momentum"]), ActionData.OperatorType.INCREASE)
