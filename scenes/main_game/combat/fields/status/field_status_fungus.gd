9c3fbcbbfe258a2694d8780b78ed56028b21736c
Chris Yang <cyang8954.game@gmail.com>
2026-04-13 08:51:28 -0700

    fungus type

--- scenes/main_game/combat/fields/status/field_status_fungus.gd
+++ scenes/main_game/combat/fields/status/field_status_fungus.gd
@@ -16,7 +16,7 @@ func _update_for_plant(plant:Plant) -> void:
 			fungus.position = _get_random_fungus_position(plant)
 	elif diff_number_of_fungi < 0:
 		var fungi := container.get_children()
-		var fungi_to_remove := abs(diff_number_of_fungi)
+		var fungi_to_remove:int = abs(diff_number_of_fungi)
 		for i in fungi_to_remove:
 			var fungus:Fungus = fungi[fungi.size() - 1 - i]
 			fungus.queue_free()
