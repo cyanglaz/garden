7aef5f1c67b0641208ab940e11b458cf08f8d758
Chris Yang <cyang8954.game@gmail.com>
2026-04-14 20:44:21 -0700

    rewview fixes

--- scenes/main_game/combat/fields/gui_field_status_container.gd
+++ scenes/main_game/combat/fields/gui_field_status_container.gd
@@ -11,18 +11,15 @@ func bind_with_field_status_container(field_status_container:FieldStatusContaine
 func _on_status_updated(field_status_container:FieldStatusContainer) -> void:
 	Util.remove_all_children(self)
 	for field_status:FieldStatus in field_status_container.get_active_statuses():
-		if !field_status.active:
-			continue
 		var status_icon:GUIFieldStatusIcon = STATUS_ICON_SCENE.instantiate()
 		add_child(status_icon)
 		status_icon.setup_with_field_status_data(field_status.status_data, field_status.stack)
 	
 func _on_status_hook_animation_requested(status_id:String) -> void:
-	var animating_icon:GUIFieldStatusIcon
+	var animating_icon:GUIFieldStatusIcon = null
 	for status_icon:GUIFieldStatusIcon in get_children():
 		if status_icon.status_id == status_id:
 			animating_icon = status_icon
 	if animating_icon == null:
 		return
-	assert(animating_icon !=null, "Animating icon not found")
 	animating_icon.play_trigger_animation()
