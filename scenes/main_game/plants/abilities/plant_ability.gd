988ed406c628c2e11ebe0bff25fcb7fbd24a108c
Chris Yang <cyang8954.game@gmail.com>
2026-04-14 20:50:13 -0700

    queued ability should trigger

--- scenes/main_game/plants/abilities/plant_ability.gd
+++ scenes/main_game/plants/abilities/plant_ability.gd
@@ -10,6 +10,8 @@ var current_cooldown:int = 0: set = _set_current_cooldown
 var active:bool = true
 
 func has_ability_hook(ability_type:Plant.AbilityType, plant:Plant, combat_main:CombatMain) -> bool:
+	if not active:
+		return false
 	if current_cooldown > 0:
 		if ability_type == Plant.AbilityType.START_TURN:
 			current_cooldown -= 1
@@ -17,14 +19,14 @@ func has_ability_hook(ability_type:Plant.AbilityType, plant:Plant, combat_main:C
 	return _has_ability_hook(ability_type, plant, combat_main)
 
 func trigger_ability_hook(ability_type:Plant.AbilityType, plant:Plant) -> void:
+	if not active:
+		return
 	var request = CombatQueueRequest.new()
 	request.front = true
 	request.callback = func(cm:CombatMain) -> void: _handle_trigger_ability_hook(ability_type, plant, cm)
 	Events.request_combat_queue_push.emit(request)
 
 func _handle_trigger_ability_hook(ability_type:Plant.AbilityType, plant:Plant, combat_main:CombatMain) -> void:
-	if not active:
-		return
 	request_ability_hook_animation.emit(ability_data.id)
 	_trigger_ability_hook(ability_type, plant, combat_main)
 	current_cooldown = ability_data.cooldown
