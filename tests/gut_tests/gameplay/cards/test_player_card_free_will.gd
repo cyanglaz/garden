cbd64c48755d0e0b0f745f3e6f228cae90975125
Chris Yang <cyang8954.game@gmail.com>
2026-04-17 06:46:23 -0700

    cleanup

--- tests/gut_tests/gameplay/cards/test_player_card_free_will.gd
+++ tests/gut_tests/gameplay/cards/test_player_card_free_will.gd
@@ -20,10 +20,8 @@ func _make_combat_main() -> FakeCombatMain:
 func test_number_of_secondary_cards_to_select_is_one() -> void:
 	assert_eq(ToolScriptFreeWill.new().number_of_secondary_cards_to_select(), 1)
 
-
 func test_get_card_selection_type_is_non_restricted() -> void:
 	assert_eq(ToolScriptFreeWill.new().get_card_selection_type(), ActionData.CardSelectionType.NON_RESTRICTED)
-
 
 func test_apply_tool_adds_reversible_to_selected_card() -> void:
 	var selected := _make_tool_data()
