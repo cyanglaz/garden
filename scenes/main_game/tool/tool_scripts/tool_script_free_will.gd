264e9a05df4fc6726721da76269ed8d63f64dc06
Chris Yang <cyang8954.game@gmail.com>
2026-04-17 09:16:36 -0700

    review fixes

--- scenes/main_game/tool/tool_scripts/tool_script_free_will.gd
+++ scenes/main_game/tool/tool_scripts/tool_script_free_will.gd
@@ -6,15 +6,16 @@ func apply_tool(combat_main: CombatMain, _tool_data: ToolData, secondary_card_da
 		return
 	var selected_card: ToolData = secondary_card_datas[0]
 	var changed := false
-	if !selected_card.specials.has(ToolData.Special.REVERSIBLE):
-		selected_card.specials.append(ToolData.Special.REVERSIBLE)
-		changed = true
-	if selected_card.back_card and !selected_card.back_card.specials.has(ToolData.Special.REVERSIBLE):
-		selected_card.back_card.specials.append(ToolData.Special.REVERSIBLE)
-		changed = true
-	if selected_card.front_card and !selected_card.front_card.specials.has(ToolData.Special.REVERSIBLE):
-		selected_card.front_card.specials.append(ToolData.Special.REVERSIBLE)
-		changed = true
+	var faces_to_update = [selected_card]
+	if selected_card.back_card:
+		faces_to_update.append(selected_card.back_card)
+	if selected_card.front_card:
+		faces_to_update.append(selected_card.front_card)
+
+	for face in faces_to_update:
+		if !face.specials.has(ToolData.Special.REVERSIBLE):
+			face.specials.append(ToolData.Special.REVERSIBLE)
+			changed = true
 	if changed:
 		selected_card.refresh_ui(combat_main)
 	await Util.await_for_tiny_time()
