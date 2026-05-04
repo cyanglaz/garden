d7a62b25afd0bec4e87f25c01b3af97587702651
Chris Yang <cyang8954.game@gmail.com>
2026-05-03 17:35:16 -0700

    update recharge and refill

--- scenes/main_game/tool/tool_scripts/tool_script_recharge.gd
+++ scenes/main_game/tool/tool_scripts/tool_script_recharge.gd
@@ -1,28 +1,10 @@
 class_name ToolScriptRecharge
 extends ToolScript
 
-func apply_tool(combat_main:CombatMain, _tool_data:ToolData, secondary_card_datas:Array) -> void:
-	await Util.await_for_tiny_time()
-	for tool_data:ToolData in secondary_card_datas:
-		var battery_tool_datas := []
-		if tool_data.id == "solar_battery":
-			battery_tool_datas.append(tool_data)
-		assert(battery_tool_datas.size() > 0, "Recharge can only select solar battery")
-		for battery_tool_data in battery_tool_datas:
-			var light_action_data:ActionData  = battery_tool_data.actions[0]
-			assert(light_action_data.type == ActionData.ActionType.LIGHT, "Solar battery's first action is light action")
-			light_action_data.modified_x_value = 0
-			battery_tool_data.refresh_ui(combat_main)
-
-func number_of_secondary_cards_to_select() -> int:
-	return 1
-
-func secondary_card_selection_filter() -> Callable:
-	return func(tool_data:ToolData) -> bool:
-		return tool_data.id == "solar_battery"
-
-func get_card_selection_type() -> ActionData.CardSelectionType:
-	return ActionData.CardSelectionType.RESTRICTED
-
-func get_card_selection_custom_error_message() -> String:
-	return Util.get_localized_string("WARNING_NO_CARD_IN_HAND") % "solar_battery"
+func apply_tool(combat_main:CombatMain, _tool_data:ToolData, _secondary_card_datas:Array) -> void:
+	var all_solar_batteries:Array = combat_main.tool_manager.tool_deck.hand.filter(func(tool_data:ToolData) -> bool: return tool_data.id == "solar_battery")
+	for solar_battery_tool_data in all_solar_batteries:
+		var light_action_data:ActionData  = solar_battery_tool_data.actions[0]
+		assert(light_action_data.type == ActionData.ActionType.LIGHT, "Solar battery's first action is light action")
+		light_action_data.modified_x_value = 0
+		solar_battery_tool_data.refresh_ui(combat_main)
--- scenes/main_game/tool/tool_scripts/tool_script_refill.gd
+++ scenes/main_game/tool/tool_scripts/tool_script_refill.gd
@@ -1,20 +1,11 @@
 class_name ToolScriptRefill
 extends ToolScript
 
-func apply_tool(combat_main:CombatMain, _tool_data:ToolData, secondary_card_datas:Array) -> void:
-	var selected_tool_data:ToolData = secondary_card_datas.front()
+func apply_tool(combat_main:CombatMain, _tool_data:ToolData, _secondary_card_datas:Array) -> void:
+	var all_empty_bottles:Array = combat_main.tool_manager.tool_deck.hand.filter(func(tool_data:ToolData) -> bool: return tool_data.id == "empty_bottle")
 	var bottled_water_tool_data:ToolData = MainDatabase.tool_database.get_data_by_id("bottled_water").get_duplicate()
-	if selected_tool_data.id == "empty_bottle":
-		selected_tool_data.copy(bottled_water_tool_data)
-	combat_main.tool_manager.update_tool_card(selected_tool_data, selected_tool_data)
+	for empty_bottle_tool_data in all_empty_bottles:
+		empty_bottle_tool_data.copy(bottled_water_tool_data)
+		combat_main.tool_manager.update_tool_card(empty_bottle_tool_data, bottled_water_tool_data)
 	await Util.await_for_tiny_time()
 
-func number_of_secondary_cards_to_select() -> int:
-	return 1
-
-func secondary_card_selection_filter() -> Callable:
-	return func(tool_data:ToolData) -> bool:
-		return tool_data.id == "empty_bottle"
-
-func get_card_selection_type() -> ActionData.CardSelectionType:
-	return ActionData.CardSelectionType.RESTRICTED
