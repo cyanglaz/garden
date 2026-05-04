0c4a07e9f09473bafb4c557a792c6df6b23671a1
Chris Yang <cyang8954.game@gmail.com>
2026-05-03 18:28:48 -0700

    review fixse

--- scenes/main_game/tool/tool_scripts/tool_script_refill.gd
+++ scenes/main_game/tool/tool_scripts/tool_script_refill.gd
@@ -5,7 +5,6 @@ func apply_tool(combat_main:CombatMain, _tool_data:ToolData, _secondary_card_dat
 	var all_empty_bottles:Array = combat_main.tool_manager.tool_deck.hand.filter(func(tool_data:ToolData) -> bool: return tool_data.id == "empty_bottle")
 	var bottled_water_tool_data:ToolData = MainDatabase.tool_database.get_data_by_id("bottled_water").get_duplicate()
 	for empty_bottle_tool_data in all_empty_bottles:
-		empty_bottle_tool_data.copy(bottled_water_tool_data)
 		combat_main.tool_manager.update_tool_card(empty_bottle_tool_data, bottled_water_tool_data)
 	await Util.await_for_tiny_time()
 
